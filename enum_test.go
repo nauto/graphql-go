c84d025776e87cfdc204f9a2f61de5035a36a37a
Denis Trapeznikov <denis.trapeznikov@nauto.com>
2019-03-14 00:51:30 +0100

    go fmt applied.

--- enum_test.go
+++ enum_test.go
@@ -174,7 +174,7 @@ func TestInvalidEnum(t *testing.T) {
 			query($justso: String!) {
 				crash(why: $justso)
 			}`,
-			Variables: map[string]interface{}{"justso": ""},
+			Variables:      map[string]interface{}{"justso": ""},
 			ExpectedResult: `{ "crash": "When, !" }`,
 		},
 	})
